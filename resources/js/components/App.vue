18ee30c04c8b84edb8e0b5c0e6e02554b602dd47
Lucas Aguiar <de.lucas73@gmail.com>
2025-03-23 11:02:16 -0300

    Inicializando repositorio

---
+++ resources/js/components/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Vue dentro do Laravel com Vite!</h1>
+        <button @click="contar">Cliquei {{ contador }} vezes</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            contador: 0
+        };
+    },
+    methods: {
+        contar() {
+            this.contador++;
+        }
+    }
+};
+</script>
